b20cd0b9ccc26ad1ea29d238d80f491f42017797
Numaer Zaker <znzaker@gmail.com>
2016-06-19 02:21:00 -0400

    push swift files

--- ShareEcon/ShareViewController.swift
+++ ShareEcon/ShareViewController.swift
@@ -9,28 +9,44 @@
 import UIKit
 import ArcGIS
 
-class ShareViewController: UIViewController {
+class ShareViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var textShare: UITextField!
+    
+    @IBOutlet weak var addShare: UIButton!
+    
+    @IBOutlet weak var tableShare: UITableView!
+    
+    var data = ["Shovel"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableShare.dataSource = self
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func addSharePress(sender: AnyObject) {
+        if textShare.text != ""{
+            data.append(textShare.text!)
+            textShare.text = ""
+            tableShare.reloadData()
+        }
+        
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: nil)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- ShareEcon/ViewController.swift
+++ ShareEcon/ViewController.swift
@@ -12,53 +12,25 @@ import ArcGIS
 class ViewController: UIViewController {
 
     @IBOutlet weak var tabControll: UISegmentedControl!
-    
-
- 
-    
-
     @IBOutlet weak var BorrowView: UIView!
-    
     @IBOutlet weak var ShareView: UIView!
-    var data = ["Shovel"]
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       // tableView.dataSource = self
-        // Do any additional setup after loading the view, typically from a nib.
     }
-    /*
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .Default, reuseIdentifier: nil)
-        cell.textLabel?.text = data[indexPath.row]
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
-    }
-*/
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     @IBAction func changedTab(sender: UISegmentedControl) {
-
         if (tabControll.selectedSegmentIndex == 0){
-  
             ShareView.hidden = true
             BorrowView.hidden = false
-            data.append("Dicks")
-            //tableView.reloadData()
         }
         else if(tabControll.selectedSegmentIndex == 1){
             ShareView.hidden = false
             BorrowView.hidden = true
-        
-    
-            //ßtableView.hidden = false
         }
     }
 
